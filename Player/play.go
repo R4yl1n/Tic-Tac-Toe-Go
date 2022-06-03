40b2f22b16898d51d52fcae9f9b67cee640ea061
R4yl1n <92576889+R4yl1n@users.noreply.github.com>
2022-06-03 20:32:36 +0200

    adding Pointer
    to change global variables

--- Player/play.go
+++ Player/play.go
@@ -9,18 +9,18 @@ type Player struct {
 	symbole string
 }
 
-func Play(x Player, Fields []string) {
+func Play(x Player, Fields *[]string) {
 	var validInput bool
 	for validInput = false; validInput != true; {
 		fmt.Printf("Player %v where do u wanna place ur symbole %v \n", x.Player, x.symbole)
 		var field int
 		fmt.Scan(&field)
-		if field > 9 || Fields[field-1] == "O" || Fields[field-1] == "X" {
+		if field > 9 || (*Fields)[field-1] == "O" || (*Fields)[field-1] == "X" {
 			validInput = false
 
 		} else {
 			validInput = true
-			Fields[field-1] = x.symbole
+			(*Fields)[field-1] = x.symbole
 
 		}
 	}
